c92f1b51c3063e5d01c14ca1cae2a6a1095e717f
EfosaE <osamwonyiefosa02@gmail.com>
2023-09-29 23:23:57 +0100

    i did back the react.lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home";
 import { Routes, Route } from "react-router-dom";
-import Register from "./Pages/Register/Register";
+import { Audio } from "react-loader-spinner";
 
-function App() {
+const LazyContact = React.lazy(() => import("./Pages/Contact/Contact"));
+const LazyRegister = React.lazy(() => import("./Pages/Register/Register"));
+
+function App() {    
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/Contact/Contact"
+          element={
+            <React.Suspense fallback={<Audio/>}>
+              <LazyContact />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/Register/Register"
+          element={
+            <React.Suspense fallback={<Audio/>}>
+              <LazyRegister/>
+            </React.Suspense>
+          }
+        />
       </Routes>
     </>
   );
